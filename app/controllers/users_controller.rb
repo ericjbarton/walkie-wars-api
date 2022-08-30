67450305bc0cd14373afd53dc3bb0a0a5c9b6101
ericjbarton <erockbart@gmail.com>
2022-08-30 12:32:15 -0400

    adds users controller, create and index

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      # password_confirmation: params[:password_confirmation],
+      dog_name: params[:dog_name],
+      photo_url: params[:photo_url],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def index
+    users = User.all.order(:id)
+    render json: users
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+users = User.create(name: "EJ", email: "test@example.com", password: "password", dog_name: "Brownie", photo_url: "url")
